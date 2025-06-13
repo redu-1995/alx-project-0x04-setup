4896bf0eb4784c9002bb8e509a3d7fd502fb7e0a
Rediet Abreham <nara1415karol@gmail.com>
2025-06-13 13:44:39 +0300

    creating a folder  alx-project-0x05

---
+++ alx-project-0x05/pages/_app.tsx
@@ -0,0 +1,15 @@
+// pages/_app.tsx
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import { CountProvider } from "@/context/CountContext";
+import Layout from "@/components/layouts/Layout";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
+}
